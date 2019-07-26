8d502c04f3e950ac00ac4f6c4f7a35c3427ce8f2
Ariel Hurdle <arielhurdle@gmail.com>
2019-07-26 15:30:06 +0100

    start of launch p2p server (launchsrv)

--- src/p2p/p2p.cpp
+++ src/p2p/p2p.cpp
@@ -1,1 +1,32 @@
-
+#include <src/config.h>
+#incluce "peerlist.cpp"
+namespace p2p {
+  bool launchsrv() {
+    uint64_t peerIdTemp = genPeerId();
+    #define peerId = peerIdTemp
+    uint64_t peerList = peerlist::getPeerlist();
+    if (peerList.size() = 0) { // We dont have any peers
+      if (config::seedNodes.size() = 0 && peerList.size() = 0) {
+         throw std::runtime_error("No Peers or seednodes found, exiting...");
+         return false;
+      }
+      for (uint8_t i = 0; i < config::seedNodes.size(); i++) { 
+        peerlist::addPeer(config:seedNodes[i]); // Add seed nodes as peers
+      }
+    }
+    uint64_t peerCount = peerList.size();
+    for (uint64_t ii = 0; ii < peerList.size(); i++) {
+      uint64_t connectCatch = p2p::createConnection(peerList[i]); // Connect to peers
+      if (connectCatch = 0) {
+        peerCount = (peerCount - 1);
+      }
+    }
+    if (peerCount > 0) {
+      std::cout << "Connected to " << peerCount << " peers out of " << peerList.size() << " in peerlist";
+      return true;
+    } else{
+       std::cout << "Failed to connect to any peers out of " << peerList.size() << " in peerlist";
+      return false;
+    }
+  }
+}
