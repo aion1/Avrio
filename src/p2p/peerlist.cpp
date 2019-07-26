bf6b7f9dace4f00c1b5dc027d6c92142d5ce4fda
Ariel Hurdle <arielhurdle@gmail.com>
2019-07-26 15:20:47 +0100

    add addPeer() funct
    and some minor bug fixes & changes

--- src/p2p/peerlist.cpp
+++ src/p2p/peerlist.cpp
@@ -1,14 +1,23 @@
-#include peerlist.h
-
-uint8_t getPeerlist() {
-  //uint8_t ip = (uint8_t*)malloc(8); // No longer used (yet)
-  folder = getFolder();
-  ifstream file(folder + "peerlist.txt");
-    if(file.is_open())
-    {
-        std::uint32_t peerList[];
-
-        while(file >> peerList[i]; i++;)
+#include "peerlist.h"
+namespace peerlist {
+  uint8_t getPeerlist() {
+    //uint8_t ip = (uint8_t*)malloc(8); // No longer used (yet)
+    folder = getFolder();
+    ifstream file(folder + "peerlist.txt");
+      if(file.is_open())
+      {
+          std::uint32_t peerList[];
+          while(file >> peerList[i]; i++;)
+      }
+      return PeerList;
+  }
+  void addPeer(peer) {
+    folder = getFolder();
+    ifstream file(folder + "peerlist.txt");
+    if(file.is_open()) {
+      peer >> file;
     }
-    return PeerList
-}
+    std::cout << "INFO: Succsessfully Added peer: " << peer;
+    return;
+  }
+}  
