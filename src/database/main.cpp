3e11169d0f9852d1dc0567f1a989c978275d6cea
Ariel Hurdle <arielhurdle@gmail.com>
2019-07-31 13:49:54 +0100

    Update main.cpp

--- src/database/main.cpp
+++ src/database/main.cpp
@@ -3,23 +3,33 @@
 #include <core/blockchain.h>
 #include <core/block.h>
 #include <iostream.h>
-
+#include <string>
+#include <src/utils/initilised.cpp> // TODO
 
 void main() {
-  std:;cout << "Opening DB";
-  heightIndex = getBlockHeight;
-  if (heightIndex == 0) {
-    Blockchain::Blockchain()
-  std::cout << "INFO: Current Block Index " + heightIndex;
+  if (notInitilised("database") {
+  	std::cout << "Opening DB";
+  	if (!openDatabase()) {
+		exit();
 	}
+  	heightIndex = getBlockHeight();
+  	if (heightIndex == 0) {
+    		Blockchain::Blockchain()
+  	}else{
+  		std::cout << "INFO: Current Block Index " + heightIndex;
+  	}
+  	setInitilised("database"); // TODO
+  }
 }
-	
+
 char* getFolder(){
   char* folder = calloc(256,1);
   if (DB_FOLDER == "") {
-	  snprintf(folder, 256, "~/.%s/", CURRENCY_NAME);
-  }else{
+  	snprintf(folder, 256, "~/.%s/", CURRENCY_NAME);
+  }else {
     snprintf(folder, 256 DB_FOLDER)
-	}
+  }
   return(folder);
 }
+
+	
