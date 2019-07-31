47440eff262e07262dde56adf899fa025a00e78c
Ariel Hurdle <arielhurdle@gmail.com>
2019-07-31 13:47:01 +0100

    Start flush.cpp
    This will be finished once i decide on a DB system (eg myql or lmbd) and code write.cpp and read.cpp 👍

---
+++ src/database/flush.cpp
@@ -0,0 +1,23 @@
+// Copyright Ariel hurdle 2019
+/* 
+- This File Takes the blocks stored in ram (other than the top (latest) block) and moves them to the database instead,
+- this is repeated every 6 minutes ( depending on the amount of txs in the main 
+- chain roughtly 2 MAINCHAIN blocks) or if the ammount of blocks currently stored in 
+- the ram is over 100 (this can be changed on devices using the --max-ram-blocks flag 
+- when launching arieldaemon.
+*/
+
+#inlcude <src/database/write.cpp>
+#include <src/database/read.cpp>
+#include <src/database/main.cpp>
+
+void main() {
+  if (notInitilised(database) { // TODO: make a function that checks what modules are up and running
+    // We should not have called this yet, something must have gone wrong
+    exit (EXIT_FAILURE);
+  }
+  else {
+  // TODO: Lauch flsuh daemon (every 360 secconds (6 mins) saves blocks (other than top (latest) block)
+  // - to db and then removes them from RAM
+  }
+}
