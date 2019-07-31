679b1bdaa673e65b38822ef131e61c986ea91477
Ariel Hurdle <arielhurdle@gmail.com>
2019-07-31 18:18:21 +0100

    open db

--- src/database/main.cpp
+++ src/database/main.cpp
@@ -5,13 +5,18 @@
 #include <iostream.h>
 #include <string>
 #include <src/utils/initilised.cpp> // TODO
+#include <cstdio>
+#include <cstdlib>
+#include <lmdb++.h>
+
 
 void main() {
   if (notInitilised("database") {
   	std::cout << "Opening DB";
-  	if (!openDatabase()) {
-		exit();
-	}
+  	auto env = lmdb::env::create();
+        env.set_mapsize(1UL * 1024UL * 1024UL * 1024UL); /* 1 GiB */
+        std::string path = getFolder() + "blockchain.mdb";
+        env.open(path, 0, 0664);
   	heightIndex = getBlockHeight();
   	if (heightIndex == 0) {
     		Blockchain::Blockchain()
