f67ac3c84c21c904bd69bf6f4450cc543734c754
Ariel Hurdle <arielhurdle@gmail.com>
2019-07-31 17:21:04 +0100

    Update block class

--- src/core/block.h
+++ src/core/block.h
@@ -1,14 +1,16 @@
+#include <src/config.h>
+#include <src/core/blockheader.cpp> // TODO
+#include <string>
 class Block {
 public:
-    string sPrevHash;
- 
-    Block(uint32_t nIndexIn, const string &sDataIn);
-
-  
+    string PrevHash;
+    
 private:
-    uint32_t _nIndex;
-    int64_t _nNonce;
-    string _sTxIds;
-    string _sHash;
-    time_t _tTime;
+    blockheader header; // The header of the block 
+    uint32_t index; // height of this block
+    std::vector<tx_data> transactions; // All transcations in the block in a vector
+    std::string hash; // The hash of the block
+    uint64_t timestamp; // The timestamp this block was created by the node 
+    std::string data; // Extra data that can be appended to blocks (for example text or a smart contract)
+                      // - The larger this feild the higher the fee and the max size is defined in config.h
 };
