cec531a7186dc41dff10b5fdb686525ffd16fa3f
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-02 00:01:38 +0100

    Update peerlist.cpp
    * move to a vector to store peers
    * create pserlist struct

--- src/p2p/peerlist.cpp
+++ src/p2p/peerlist.cpp
@@ -1,5 +1,10 @@
 #include "peerlist.h"
 namespace peerlist {
+  struct peerlist {
+    uint8_t whitelist;
+    uint8_t greylist;
+    uint8_t blacklist
+  }
   uint8_t getPeerlist() {
     //uint8_t ip = (uint8_t*)malloc(8); // No longer used (yet)
     folder = getFolder();
@@ -7,7 +12,7 @@ namespace peerlist {
       if(file.is_open())
       {
           std::uint32_t peerList[];
-          while(file >> peerList[i]; i++;)
+          while(file >> peerList[i]; i++; )
       }
       return PeerList;
   }
@@ -17,7 +22,12 @@ namespace peerlist {
     if(file.is_open()) {
       peer >> file;
     }
+    greyList.peerList.push_back(peer);
     std::cout << "INFO: Succsessfully Added peer: " << peer;
+    if (connectPeer(peer) {
+      peerList.erase(std::remove(peerList.greylist.begin(), peerList.greylist.end(), peer), peerList.greylist.end());
+      peerList.whitelist.push_back(peer);
+    }
     return;
   }
 }  
