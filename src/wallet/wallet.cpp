17f125f33dd0c7f2ca7c0f889b6331c7321549b2
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-02 05:12:31 +0100

    Update wallet.cpp

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1,5 +1,18 @@
-#include <include/openssl/ecdsa.h>
-class Wallet {
-  public PrivateKey privateKey;
-	public PublicKey publicKey;
-	}
+#include <sodium.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <inttypes.h>
+#include "wallet.h"
+
+namespace wallet {
+  wallet_t* generateWallet(void) {
+    wallet_t *wallet = malloc(sizeof(wallet_t));
+    wallet->balance = 0;
+     return wallet;
+  }
+}
+
+	
