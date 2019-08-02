e940352f1249cd2e1b677997aee5b71d5c256290
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-02 04:43:30 +0100

    def

--- src/chains/chain.h
+++ src/chains/chain.h
@@ -1,1 +1,13 @@
 
+#include <string>
+namespace walletChain{
+  class Chain {
+  public: 
+    std::string chainKey; // the public key of the chain used for recieving funds
+  private:
+    uint64_t txnCount; // number of transactions on this chain
+    uint64_t height; // The number of blocks since the genesis block and the last block
+    std::string lastBlockHash; // The hash of the lsst block
+    time_t lastBlockTimestamp; //the timestamp of the last block
+  }
+}
