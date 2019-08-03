a53034b90b9b4b360714dfab300a8da3aa682ab5
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-03 11:59:04 +0100

    remove unsed config varables

--- src/config.h
+++ src/config.h
@@ -1,12 +1,13 @@
 namespace config {
-  std::string currencyName = "currency";
-  uint64_t blockTarget = 100;
-  uint64_t maxBlockSize = 250000000; // 250kb
-  uint64_t decimalPlaces = 10;
-  uint64_t blockmajorVersion = 0;
-  uint64_t blockMinorversion = 0;
-  uint64_t minimumBlockReward = 300000000000; // 30
-  uint64_t waveLenghtMultiplyer = 150; 
-  uint32_t minFee = 1000000; // 0.0001 
-  uint32_t maxFee = 10000000; // 0.001 
+  namespace masterChain {
+    std::string currencyName = "Avrio";
+    uint64_t blockTarget = 60;
+    uint64_t maxBlockSize = 250000000; // 250kb
+    uint64_t maxTxExtraSize = 2500000; // the amount of (extra) data that can be attached to a tx (2.5 kb) (eg for smart contracts)
+    uint64_t decimalPlaces = 10;
+    uint64_t blockmajorVersion = 0;
+    uint64_t blockMinorversion = 0;
+    uint64_t blockReward = 0
+    uint32_t minFee = 1000000; // 0.001 
+  }
 }
