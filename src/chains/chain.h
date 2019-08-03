412ea1374716d0da1b2606d5da135a34c2a06359
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-03 12:42:01 +0100

    move to struct

--- src/chains/chain.h
+++ src/chains/chain.h
@@ -1,13 +1,12 @@
 
 #include <string>
 namespace walletChain{
-  class Chain {
-  public: 
+  struct Chain {
     std::string chainKey; // the public key of the chain used for recieving funds
-  private:
+    std::string privateKey; // the private key of the chain
     uint64_t txnCount; // number of transactions on this chain
     uint64_t height; // The number of blocks since the genesis block and the last block
-    std::string lastBlockHash; // The hash of the lsst block
+    std::string lastBlockHash; // The hash of the last block
     time_t lastBlockTimestamp; //the timestamp of the last block
   }
 }
