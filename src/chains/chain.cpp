598d0bdcee76351ebd6ad672c929113e3a326170
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-03 13:43:45 +0100

    create wallet chain()

--- src/chains/chain.cpp
+++ src/chains/chain.cpp
@@ -1,2 +1,22 @@
 #include <src/core/block.h>
+#include <src/util/keygen.cpp>
+#include <src/util/address.cpp>
 #include <chain.h>
+
+namespace walletChain {
+  Chain createChain(std::string privateKey=pretty::emptyKey) {
+    if (privateKey == pretty::emptyKey) { // == not set
+      privateKey = wallet::generatePrivateKey()
+    }
+    Chain chain;
+    chain.chainKey = getPublicKeyFromPrivate(privateKey); 
+    chain.txnCount = 0; 
+    chain.height = 0; 
+    chain.lastBlockHash = nullptr; 
+    chain.lastBlockTimestamp = nullptr; 
+    return chain;
+  }
+  std::string getAddress (Chain chain) {
+    return createAddress(chain.chainKey);
+  }
+}
