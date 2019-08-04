4b7504ddb9f5c56da37cb1ceb93b85fb0ebdac7c
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-04 17:24:55 +0100

    Update and rename main.cpp to storage.cpp

---
+++ src/database/storage.cpp
@@ -0,0 +1,68 @@
+#include <core/blockchain.h>
+#include <core/block.h>
+#include "storage.h"
+#include <src/utils/initilised.cpp> // TODO
+
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <stdio.h>
+#include <stdint.h>
+#include <iterator>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+
+namespace fs = boost::filesystem;
+
+char* getFolder(){
+	char* folder = calloc(256,1);
+	if (DB_FOLDER == "") {
+		snprintf(folder, 256, "~/.%s/", CURRENCY_NAME);
+	}else {
+		snprintf(folder, 256 DB_FOLDER)
+	}
+	return(folder);
+}
+bool createFolder(std::string path) {
+	fs::path P path + "/";
+	fs::create_directories(p / "db");
+	fs::create_directories(p / "Chains");
+	/* Full Node */
+	if (config.fullnode = True) {
+    	fs::create_directories(p / "Chains" / "master");
+	}
+	return true;
+}
+bool createChain(std::string path, std::string hash) {
+	fs::path P path + "/";
+	fs::create_directories(p / "Chains" / hash);
+	return true;
+}
+bool Storage::init() {
+	if (notInitilised("database") {
+		if (config.fullnode = True) {
+    		std::cout << "Opening Main Chain" << std::endl;
+      		std::string path = getFolder;
+			std::ifstream blockF;
+   		    blockF.open("blocks.bin");
+			// Format
+			/*
+			height
+			blockhash
+			previous block hash
+			transactions
+			timestamp
+			extra data;
+			*/
+  	  	}		
+	}
+}
+
+	
