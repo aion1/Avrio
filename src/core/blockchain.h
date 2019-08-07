51f214ed0924ebd4a16b57ca830fccacab44ef60
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-07 21:02:30 +0100

    add latest functions

--- src/core/blockchain.h
+++ src/core/blockchain.h
@@ -1,11 +1,11 @@
 class Blockchain {
 public:
-    Blockchain();
- 
-    void AddBlock(Block bNew);
- 
-private:
     vector<Block> Chain;
- 
+    static const fork_heights hard_forks[];
+    Blockchain();
+    void AddBlock(Block block);
+    uint64_t getHeight();
+    uint8_t getVersion(uint64_t height);
     Block _GetLastBlock() const;
+    Block getBlockDataForHeight(uint64_t height);
 };
