8667deaa8e30e31c2b31c772bbb1e552242a10ad
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-07 20:33:02 +0100

    typo fixes, synitax fixes and some logic fixes

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -3,38 +3,44 @@
 #include <ip2string.h>
 #include <src/msg/CliCommon.cpp>
 #include <src/P2p/p2p.h>
+#include <string>
+
 void printGenesisTx(std::string rewardAddress) {
     uint64_t  reward = config::PREMINE_AMMOUNT;
     if (reward == 0) {
-        uint64_t  reward = getBlockReward();
-        tx = buildCoinbaseTx(rewardAddress, reward);
+        reward = getBlockReward();
+        Transaction tx = buildCoinbaseTx(rewardAddress, reward);
+        crypto::hash(tx);
     }else {
-     tx = buildCoinbaseTx(rewardAddress, reward);   
+        Transaction tx = buildCoinbaseTx(rewardAddress, reward);   
+        crypto::hash(tx);
     }
-    std:;cout << "Genesis TX hash: \n";
+    std::cout << "Genesis TX hash: \n";
     std::cout << tx + "\n";
     return;
 }
 
 void main() {
-    cli::cliMain();
+    cli::cliMain(); // TODO (a "header" that is printed at the begining of every program that includes a ascii art and a license
     std::cout << "Starting node";
     #ifdef useCheckpoints
     std::cout << "Loading checkpoints for faster sync";
     #ifdef internalCheckpoints
-    for (uint64_t i = 0; i < checkpoints.size(); i++
+    for (uint64_t i = 0; i < checkpoints.size(); i++) {
         std::string errorCatch = checkpoints::load(checkPoints[i].hash, checkPoints[i].height)
-        if (errorCatch != true) {
+        if (errorCatch != '') {
             std::cout << "ERROR loading checkpoints: " << errorCatch;
             break;
        }   
     }
     std::cout << "INFO: Loaded " << checkpoints.size() << " checkpoints";
+    #endif
+    #endif
     std::cout << "INFO: Launching P2p server";
     if (!p2p::launchsrv()) {
         // Launch failed, abort
         throw std::runtime_error("Failed to start P2p server, exiting...");
-   }else {
+    }else {
         std::cout << "P2p server running with peer ID: " << p2p::getId();
         //TODO: finish daemon code
    }                       
