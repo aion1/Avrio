a58409de62e3409f8c76daec8ccb10abefefb0c4
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-07 10:32:42 +0100

    fix some synitax + remove PoW refrences

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,43 +1,31 @@
 #include <iostream.h>
 #include <math.h>
+#include "block.h"
 
 namespace Core 
 {
-  bool verify(block) 
+  bool verifyBlock(Block block) 
   {
     uint64_t hash = malloc(150,1);
     uint64_t salt, height, timestamp, txIds;
-    hash = block[0];
-    nonce = block[1];
-    height = block[2];
-    timestamp = block[3];
-    txIds = block[4];
-    uint64_t difficulty = getDifficulty(height);
-    currentHeight = getBlockchainHeight;
-    testHash = crypto::hash(block, diffiuclty);
-    if (testHash != hash) {
-       std::cout << "ERROR: Incorrect blockhash for block " + hash + "\n";
-       std::cout << "Proposed hash: " + hash + "\n";
-       std::cout << "Calculated Hash: " + testHash + "\n";
-       return false;
-       break;
-    }elif (!checkDifficulty(hash, difficulty) {
-      std::cout << "ERROR: Proof of work to weak for block: " + hash;
-      return false;
-      break;
-   }elif (height != currentHeight + 1) {
+    hash = block.hash;
+    height = block.index;
+    timestamp = block.header.timestamp;
+    txIds = block.transactions;
+    currentHeight = Blockchain::getHeight();
+    if (height != currentHeight + 1) {
       std::cout << "ERROR: height missmatch for block: " + hash + "\n";
       std::cout << "expected height: " + currentHeight;
       std::cout << "Provided Height: " + height;
       return false;
       break;
-   }elif (hash in _vChain) { // TODO: if hash in _vChain needs proper synitax.
-     std:;cout << "ERROR: Block " + hash + "allready exists!";
+   }elif (hash in chain) { // TODO: if hash in chain needs proper synitax.
+     std::cout << "ERROR: Block " + hash + "already exists!";
      return false;
      break;
    }else {
       return true;
-    }
+   }
   }
   uint64_t getBlockReward(height)
   {
