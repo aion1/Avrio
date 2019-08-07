50e22d590f0d246cce1fe84ad18634bdb885e1d7
Ariel Hurdle <arielhurdle@gmail.com>
2019-08-07 10:23:42 +0100

    remove data thats allready in block header

--- src/core/block.h
+++ src/core/block.h
@@ -7,9 +7,7 @@ public:
     Blockheader header; // The header of the block 
     uint32_t index; // height of this block
     std::string hash;  // The hash of the block
-    std::string prevhash;
     std::vector<tx_data> transactions; // All transcations in the block in a vector
-    uint64_t timestamp; // The timestamp this block was created by the node 
     std::string data; // Extra data that can be appended to blocks (for example text or a smart contract)
                       // - The larger this feild the higher the fee and the max size is defined in config.h
 };
